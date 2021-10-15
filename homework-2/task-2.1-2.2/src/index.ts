c8ec25ae23479ddb1f674b33d5e37c82f66ffe38
Ruslan Bakanov <Ruslan_Bakanov@epam.com>
2021-10-15 22:58:43 +0300

    express: router created

--- homework-2/task-2.1-2.2/src/index.ts
+++ homework-2/task-2.1-2.2/src/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import * as routes from "./routes";
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+// Configure routes
+routes.register(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
