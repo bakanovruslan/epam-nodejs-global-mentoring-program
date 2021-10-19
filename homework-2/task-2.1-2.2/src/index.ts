f63d88b6891a1656dbdf9b50d05de3293d54949c
Ruslan Bakanov <Ruslan_Bakanov@epam.com>
2021-10-19 22:09:13 +0300

    auto suggest list action

--- homework-2/task-2.1-2.2/src/index.ts
+++ homework-2/task-2.1-2.2/src/index.ts
@@ -4,9 +4,8 @@ import * as routes from "./routes";
 const app = express();
 const port = 3000;
 
-// Configure routes
 routes.register(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
--- homework-2/task-2.1-2.2/src/routes/index.ts
+++ homework-2/task-2.1-2.2/src/routes/index.ts
@@ -1,7 +1,23 @@
 import * as express from "express";
-// import url from 'url';
 
-//TODO: replace any
+//TODO: incapsulate params in config module
+const userLimit = 20;
+
+//TODO: incapsulate functions in functions module
+//TODO: replace any types
+function getAutoSuggestUsers(loginSubstring: any, limit: any) {
+    let result = usersContainer;
+    //TODO: replace any types
+    result = result.filter((obj: any) => {
+        return obj.login.includes(loginSubstring);
+    })
+    //TODO: replace any types
+    result.sort((a: any, b: any) => a.login.localeCompare(b.login));
+    return result.slice(0, limit);
+}
+
+
+//TODO: replace any types
 function searchUser(key: any, arr: any) {
     for (var i = 0; i < arr.length; i++) {
         if (arr[i].id === key) {
@@ -10,25 +26,25 @@ function searchUser(key: any, arr: any) {
     }
 }
 
-//TODO: replace any type
+//TODO: replace any types
 function updateUser(id: string, params: any, box: []) {
     let user = searchUser(id, box);
     if (user) {
         //TODO: validation for all params
         if (params.id) {
-            user.id = params.id;    
+            user.id = params.id;
         }
         if (params.login) {
-            user.login = params.login;    
+            user.login = params.login;
         }
         if (params.password) {
-            user.password = params.password;    
+            user.password = params.password;
         }
         if (params.age) {
-            user.age = params.age;    
+            user.age = params.age;
         }
         if (params.isDeleted) {
-            user.isDeleted = params.isDeleted;    
+            user.isDeleted = params.isDeleted;
         }
     } else {
         //TODO: if empty case
@@ -38,9 +54,7 @@ function updateUser(id: string, params: any, box: []) {
 
 }
 
-/**
- * TODO: replace any
- */
+//TODO: replace any types
 let usersContainer: any = [];
 
 type User = {
@@ -56,6 +70,7 @@ export const register = (app: express.Application) => {
     /**
      * Create user 
      */
+    //TODO: replace any types
     app.post("/users", (req: any, res) => {
         let user: User = {
             id: req.query.id,
@@ -70,15 +85,6 @@ export const register = (app: express.Application) => {
     });
 
     /**
-     * Get user
-     */
-    app.get("/users/:userId", (req, res) => {
-        let result = searchUser(req.params.userId, usersContainer);
-        //TODO: if empty case
-        res.json(result);
-    });
-
-    /**
      * Update user
      */
     app.patch("/users/:userId", (req, res) => {
@@ -87,5 +93,27 @@ export const register = (app: express.Application) => {
         let user = updateUser(id, params, usersContainer);
         res.json(user);
     });
+
+    /**
+     * Auto-suggested list (filtered by substring)
+     */
+    app.get("/users/list", (req, res) => {
+        console.log(req.query);
+        if (req.query.search) {
+            let result = getAutoSuggestUsers(req.query.search, userLimit);
+            res.json(result);
+        }
+        res.end();
+    });
+
+    /**
+     * Get user
+     */
+    app.get("/users/:userId", (req, res) => {
+        let result = searchUser(req.params.userId, usersContainer);
+        //TODO: if empty case
+        res.json(result);
+    });
+
 
 };
