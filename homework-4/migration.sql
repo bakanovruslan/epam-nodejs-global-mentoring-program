77a38572f957040750f88ef8e27bc224e15d3144
Ruslan Bakanov <Ruslan_Bakanov@epam.com>
2021-11-19 22:34:49 +0200

    changes for demo PR

--- homework-4/migration.sql
+++ homework-4/migration.sql
@@ -1,11 +1,11 @@
-create type permission as enum ('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES');
-
-CREATE TABLE groups(
-    id int PRIMARY KEY,
-    name varchar(255),
-    permissions permission[]
-);
-
+create type permission as enum ('READ', 'WRITE', 'DELETE', 'SHARE', 'UPLOAD_FILES'); 
+ 
+CREATE TABLE groups( 
+    id int PRIMARY KEY, 
+    name varchar(255), 
+    permissions permission[] 
+); 
+ 
 CREATE TABLE user_group (
     user_id int,
     group_id int,
