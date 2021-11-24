afe736f0ff3869031d0637a7a40478573a5c813d
Ruslan Bakanov <Ruslan_Bakanov@epam.com>
2021-11-24 20:37:20 +0200

    homework-5: started

---
+++ homework-5/task-5.1-5.3/src/services/GroupService.ts
@@ -0,0 +1,33 @@
+import { Group } from '../types/Group';
+class GroupService {
+
+    private groupModel;
+
+    constructor(groupModel: any) {
+        this.groupModel = groupModel;
+    }
+
+    async createGroup(user: Group) {
+        let result = await this.groupModel.create(user);
+        return result.toJSON();
+    }
+
+    async updateGroup(groupId: string, data: object) {
+        await this.groupModel.update(data, { where: { id: groupId } });
+        return await this.groupModel.findByPk(groupId);
+    }
+
+    async getGroup(groupId: number) {
+        return await this.groupModel.findByPk(groupId);
+    }
+
+    async getAllGroups() {
+        return await this.groupModel.findAll();
+    }
+
+    async removeGroup(groupId: number) {
+        return await this.groupModel.destroy({ where: { id: groupId } });
+    }
+
+}
+export { GroupService };
