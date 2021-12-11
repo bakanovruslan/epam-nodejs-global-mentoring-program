3b651965fdb8cc993694a6b987e6218f431ae171
Ruslan Bakanov <Ruslan_Bakanov@epam.com>
2021-12-11 21:19:15 +0200

    request logger middleware created

--- homework-5/task-5.1-5.3/src/index.ts
+++ homework-5/task-5.1-5.3/src/index.ts
@@ -3,10 +3,12 @@ import * as UserController from "./controllers/UserController";
 import * as GroupController from "./controllers/GroupController";
 import * as UserGroupController from "./controllers/UserGroupController";
 import {Config} from './config/index';
+import * as Logger from "./middlewares/app/logger";
 
 const app = express();
 const port = Config.port;
 
+Logger.register(app);
 UserController.register(app);
 GroupController.register(app);
 UserGroupController.register(app);
